9623798ffd199624e58f48dd1d6abf35dbadd9d8
Nicolas Romer <nicolasromer@gmail.com>
2016-03-18 13:41:38 -0400

    score around 15500

--- SecondGuesser.js
+++ SecondGuesser.js
@@ -1,86 +1,89 @@
+/*
+ *  MetaRand : switches strategy randomly if things are going poorly
+ */
 RPS.Player.MetaRand = function() {
 
 	// Believe in yourself!
-	this.confidence 	= 10;
+	this.confidence = 10;
 
-						// R  P  S 
-	this.oppMoveCount 	= [0, 0, 0];
-	this.myLastMove 	= 1;
+	//Keep track :       R  P  S
+	this.oppMoveCount = [0, 0, 0];
+	this.myLastMove = 1;
 };
+
 RPS.Player.MetaRand.NAME = 'MetaRand';
-RPS.Player.MetaRand.MEMORY = "";
-RPS.Player.MetaRand.STRATEGY = 'random';
+// Memory needs to carry through all 100 sets with an opponent
+RPS.Player.MetaRand.Memory = "";
+RPS.Player.MetaRand.Strategy = 'random';
 
 //Check if the recent pattern of moves has occurred in memory
-RPS.Player.MetaRand.prototype.patternMatch = function() {
+RPS.Player.MetaRand.prototype.patternMatch = function(span) {
 
-		var hist = RPS.Player.MetaRand.MEMORY;
+		var hist = RPS.Player.MetaRand.Memory,
+			prediction = undefined;
 
 		// TODO: rate range of pattern lengths based on effectiveness
-		var pattern = hist.slice(hist.length - 5);
-		var past  = hist.slice(0, -5)
+		var pattern = hist.slice(hist.length - span);
+		var past = hist.slice(0, -span);
 
 		var index = past.indexOf( pattern );
 
-		if (index != -1) {
-			var prediction = hist[index + pattern.length];
+		if (index !== -1) {
+			prediction = hist[index + pattern.length];
 		}
 
-		if (prediction != undefined) {
+		if (prediction !== undefined) {
 			return parseInt(prediction);
 		} else {
 			return RPS.randomMove();
 		}
-};
 
-RPS.Player.MetaRand.prototype.trumpMostPlayed = function(arr) {
-		var index = arr.indexOf( Math.max(...arr));
-		return RPS.trumps(index + 1);
 };
 
 RPS.Player.MetaRand.prototype.throwMove = function() {
 
-	var mem = RPS.Player.MetaRand.MEMORY,
-		strat = RPS.Player.MetaRand.STRATEGY,
+	var strat = RPS.Player.MetaRand.Strategy,
 		myMove = RPS.randomMove();
-
 	if (strat == 'frequency analysis') {
-
-		myMove = this.trumpMostPlayed(this.oppMoveCount);
-
+		myMove = trumpMostPlayed(this.oppMoveCount);
 	} else if (strat == 'pattern matching') {
-		if (mem.length > 30) {
-			debugger;
-			myMove = this.patternMatch();
-		}
+		myMove = this.patternMatch(10);
 	} else if (strat == 'trump my last move') {
-		myMove = RPS.trumps(this.myLastMove);
+			myMove = RPS.trumps(this.myLastMove);
 	}
 
+	// store the move for reference next round
 	this.myLastMove = myMove;
+
 	return myMove;
+
+	// trump the most played move
+	function trumpMostPlayed(arr) {
+			var index = arr.indexOf( Math.max(...arr));
+			return RPS.trumps(index + 1);
+	}
 
 };
 
 /*
- *	Record the results
+ *  Record the results
  */
 RPS.Player.MetaRand.prototype.addResult = function(result, opponentMove) {
 
-	RPS.Player.MetaRand.MEMORY += String(opponentMove);
+	RPS.Player.MetaRand.Memory += String(opponentMove);
 
 	// Frequency: add oppMove to round frequency
 	this.oppMoveCount[opponentMove - 1] += 1;
 
 	// adjust confidence level, biased towards losing
-	if (result == 1) {
+	if (result === 1) {
 		this.confidence += 1
 	} else {
 		this.confidence -= 2;
 	}
 	/*
 	 * TODO: replace 'confidence' with rating system for strategies 
-	 * 			testing how each would do against the match results
+	 *       testing how each would do against the match results
 	 *
 	 * TODO: establish rating system for secondGuessing
 	 */
@@ -90,17 +93,17 @@ RPS.Player.MetaRand.prototype.addResult = function(result, opponentMove) {
 		var rand = Math.floor((Math.random() * 4) + 1);
 		switch (rand) {
 			case 1:
-				RPS.Player.MetaRand.STRATEGY = 'pattern matching';
+				RPS.Player.MetaRand.Strategy = 'pattern matching';
 				break;
 			case 2:
-				RPS.Player.MetaRand.STRATEGY = 'frequency analysis';
+				RPS.Player.MetaRand.Strategy = 'frequency analysis';
 				break;
 			case 3:
-				RPS.Player.MetaRand.STRATEGY = 'trump my last move';
+				RPS.Player.MetaRand.Strategy = 'trump my last move';
 				break;				
 			default:
-				RPS.Player.MetaRand.STRATEGY = 'random';
+				RPS.Player.MetaRand.Strategy = 'random';
 		}
 		this.confidence = 5;
-	} 
+	}
 }
