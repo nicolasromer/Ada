572f405c86eefa7195a2e31bf034f009e3b86796
Nicolas Romer <nicolasromer@gmail.com>
2016-03-22 14:02:46 -0400

    increase span in elephant search

--- Elephant.js
+++ Elephant.js
@@ -1,86 +1,56 @@
     /*
-     *  MetaRand : switches strategy randomly if things are going poorly
+     *  Elephant : Keeps all 10,000 enemy moves in memory and searches for a match
      */
-    RPS.Player.MetaRand = function() {
+    RPS.Player.Elephant = function() {};
+    RPS.Player.Elephant.NAME = 'Elephant';
 
-        // Believe in yourself!
-        this.confidence = 10;
-        this.paranoia = 0;
-
-        //Keep track :       R  P  S
-        this.oppMoveCount = [0, 0, 0];
-        this.myMoveCount = [0, 0, 0];
-        this.myLastMove = 2;
-        this.oppLastMove = 1;
-
-        this.provenSpan = 0;
-
-        
-    };
-
-    RPS.Player.MetaRand.NAME = 'MetaRand';
-    // Memory needs to carry through all 100 sets with an opponent
-    RPS.Player.MetaRand.Memory = "";
-    RPS.Player.MetaRand.Strategy = 'trump my last move';
+    // Elephant remembers all 100 sets with an opponent
+    RPS.Player.Elephant.Memory = "";
 
     //Check if the recent pattern of moves has occurred in memory
-    //   Recurse with ever shorter spans
-    RPS.Player.MetaRand.prototype.patternMatch = function(span) {
+    RPS.Player.Elephant.prototype.patternMatch = function(span) {
 
-        if (span > 4 && RPS.Player.MetaRand.Memory.length > span * 2) {
-            var hist = RPS.Player.MetaRand.Memory;
+        if (span > 4 && RPS.Player.Elephant.Memory.length > span * 2) {
+
+            var hist = RPS.Player.Elephant.Memory;
             var prediction = undefined;
 
-            // TODO: rate range of pattern lengths based on effectiveness
             var pattern = hist.slice(hist.length - span);
-
-            //remove the pattern so that we don't get it in our search
             var past = hist.slice(0, -span);
+            var index = past.lastIndexOf( pattern );
 
-            //Check if our past contains the pattern and grab the index
-            var index = past.indexOf( pattern );
-
-            //If a match was found
             if (index > 0) {
-                //set this span as provenspan
-                this.provenSpan = span;
 
                 // get the move occurring after found pattern
                 prediction = hist[index + pattern.length];
                 return parseInt(prediction);
 
-            //Recurse with a shorter search span
-            } else { arguments.callee(span - 5); }
+            } else { 
+
+                //Recurse with a shorter search span
+                arguments.callee( Math.floor(span / 2) );
+
+            }
         } else {
             return RPS.randomMove();
         }
     };
 
-    RPS.Player.MetaRand.prototype.throwMove = function() {
+    RPS.Player.Elephant.prototype.throwMove = function() {
 
-        var s = RPS.Player.MetaRand.Strategy;
-        var m = RPS.randomMove();
-
-
-            m = this.patternMatch(this.provenSpan ? this.provenSpan : 25);
-
-
-        // store the move for reference
-        this.myMoveCount[m - 1] += 1;
-        this.myLastMove = m;
-
-        return m;
+        return this.patternMatch(60);
 
     };
 
     /*
      *  Record the results
      */
-    RPS.Player.MetaRand.prototype.addResult = function(result, opponentMove) {
+    RPS.Player.Elephant.prototype.addResult = function(result, opponentMove) {
 
         //Store data on my opponent
-        RPS.Player.MetaRand.Memory += String(opponentMove);
+        RPS.Player.Elephant.Memory += String(opponentMove);
 
-
+        // clear the cache every set
+        if (RPS.Player.Elephant.Memory.length > 9999) { RPS.Player.Elephant.Memory = ""; }
 
     };
--- Megazord.js
+++ Megazord.js
@@ -5,30 +5,86 @@ RPS.Player.Megazord = function() {
 
     // confidence that my current strategy is working
     this.confidence = 10;
-    this.strategies = {
-        trumpLast: 1,
-        predict: 2,
-        outwit: 3,
-        random: 4
-    };
-    this.effectiveness = [0, 0, 0, 0];
+    
+    //Keep track:       R  P  S
+    this.myMoves = [0, 0, 0];
+    this.myLastMove = 1;
+
+    //span length that found a match in memory
+    this.provenSpan = 0;
+
 };
 RPS.Player.Megazord.NAME = 'Megazord';
 RPS.Player.Megazord.Strategy = 'trumpLast';
 
+// Memory needs to carry through all 100 sets with an opponent
+RPS.Player.Megazord.Memory = "";
+
+/*
+ * Check if the recent pattern of moves has occurred in memory
+ *  Recurse with ever shorter spans
+ */
+RPS.Player.Megazord.prototype.patternMatch = function(span) {
+
+    if (span > 4 && RPS.Player.Megazord.Memory.length > span * 2) {
+        var hist = RPS.Player.Megazord.Memory;
+        var prediction = undefined;
+
+        // TODO: rate range of pattern lengths based on effectiveness
+        var pattern = hist.slice(hist.length - span);
+
+        //remove the pattern so that we don't get it in our search
+        var past = hist.slice(0, -span);
+
+        //Check if our past contains the pattern and grab the index
+        var index = past.indexOf( pattern );
+
+        //If a match was found
+        if (index > 0) {
+            //set this span as provenspan
+            this.provenSpan = span;
+
+            // get the move occurring after found pattern
+            prediction = hist[index + pattern.length];
+            return parseInt(prediction);
+
+        //Recurse with a shorter search span
+        } else { arguments.callee(span - 5); }
+    } else {
+        return RPS.randomMove();
+    }
+};
 
 RPS.Player.Megazord.prototype.throwMove = function() {
+
+    // move
+    var m;
 
     // Check the current strategy and attack
     switch (RPS.Player.Megazord.Strategy) {
             case 'trumpLast':
+            
+                // trump my last move
+                m = RPS.trumps(this.myLastMove);
                 
             case 'patternMatch':
+
+                m = this.patternMatch(this.provenSpan ? this.provenSpan : 25);
                 
             case 'owtwit':
+
+                // get the index of my most played move
+                var myMostPlayed = this.myMoves.indexOf( Math.max(...this.myMoves)) + 1;
+                // Double trump
+                m = RPS.trumps(RPS.trumps(myMostPlayed));
                 
             default:
+                m = RPS.randomMove();
     }
+
+    // store the move for reference
+    this.myMoves[m - 1] += 1;
+    this.myLastMove = m;
 
     return m;
 
@@ -39,13 +95,19 @@ RPS.Player.Megazord.prototype.throwMove = function() {
  */
 RPS.Player.Megazord.prototype.addResult = function(result, opponentMove) {
 
+    //Store data on my opponent
+    RPS.Player.Megazord.Memory += String(opponentMove);
+
+    // clear the cache every set
+    if (RPS.Player.Megazord.Memory.length > 9999) { RPS.Player.Megazord.Memory = ""; }
 
 
-
-    // adjust confidence level, drops faster when losing
+    // adjust confidence level, drops faster on loss or tie
     if (result === 1) {
         this.confidence += 1;
-    } else {this.confidence -= 3;}
+    } else {
+        this.confidence -= 3;
+    }
 
     // if confidence hits rock bottom switch strategy
     if (this.confidence < 1) {
